2a0f2dcf07d2170e1bdf9b70c5a92cfeb90fe50d
tete389 <tete389@gmail.com>
2023-01-24 02:01:45 +0700

    gh-pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { StyledEngineProvider } from "@mui/material/styles";
 import { Provider } from "react-redux";
 import { store } from "./stores/store";
 import App from "./App";
-import "./index.css";
+//import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
